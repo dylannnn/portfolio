b1b638b74f3d65a25be417fc045052b9f8267eca
Yunfei Li <dylannnnlee@gamil.com>
2015-07-02 17:49:09 +0200

    daily update
    daily update

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,25 +1,24 @@
 $(function(){
+	var html = $('html').html();
         $(".typed").typed({
-            strings: ["Fir^1000st sentence.", "Second sentence."],
-			contentType: 'html',
-            typeSpeed: 50,
+            strings: [html],
+			contentType: 'text',
+            typeSpeed: 0,
 			startDelay: 0,
             // backspacing speed
             backSpeed: 0,
             // time before backspacing
             backDelay: 500,
             // loop
-            loop: true,
+            loop: false,
             // false = infinite
             loopCount: false,
             // show cursor
-            showCursor: true,
+            showCursor: false,
             // character for cursor
             cursorChar: "|",
             // attribute to type (null == text)
             attr: null,
-            // either html or text
-            contentType: 'html',
             // call when done callback function
             callback: function() {},
             // starting callback function before each string
@@ -29,4 +28,24 @@ $(function(){
             // callback for reset
             resetCallback: function() {}
         });
+	
+		interact('.right')
+			.resizable({
+				edges: {left: true, right: false, bottom: false, top: true}
+			})
+			.on('resizimove', function () {
+				var target = event.target,
+					x = (parseFloat(target.getAttribute('data-x')) || 0 ),
+					y = (parseFloat(target.getAttribute('data-y')) || 0 );
+				target.style.width = event.rect.width + 'px';
+				target.style.height = event.rect.height + 'px';
+				x += event.deltaRect.left;
+				y += event.deltaRect.top;
+				target.style.webkitTransform = target.style.transform = 'translate(' + x + 'px,' + y + 'px)';
+
+				target.setAttribute('data-x', x);
+				target.setAttribute('data-y', y);
+				target.textContent = event.rect.width + '×' + event.rect.height;
+		});
+	
     });
